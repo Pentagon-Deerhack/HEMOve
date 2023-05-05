f45484618c4178ceb21f08e8c046ef5519be4c19
Shasmit <shasmitbasnet606@gmail.com>
2023-05-06 00:23:06 +0545

    Information separated into different lists.

--- QRcode/main.py
+++ QRcode/main.py
@@ -1,6 +1,7 @@
 #importing the module
 import numpy as np
 import cv2
+import json
 
 
 #reading the Qr code image
@@ -15,3 +16,17 @@ data, bbox, straight_qrcode = detector.detectAndDecode(img)
 #checking if the image is of a QRCode or not
 if bbox is not None:
     print(data)
+
+
+json_data = json.loads(data)
+
+
+keys = []
+values = []
+items = json_data.items()
+for item in items:
+    keys.append(item[0]), values.append(item[1])
+
+
+print("keys : ", str(keys))
+print("values : ", str(values))
