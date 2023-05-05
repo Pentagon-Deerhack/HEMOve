9ecc8b781ffefdc1986b568ae8b24f93d8a7202e
ATOIsus <sabinmaharjan884@gmail.com>
2023-05-05 21:44:45 +0545

    Test2: DHT22 test done.

--- main/main.ino
+++ main/main.ino
@@ -1,15 +1,37 @@
-int led = 13;
+//Importing libraries.
+#include <DHT.h>;
 
+
+//Defining Pins.
+#define dhtPin 2
+
+//Defining libraries.
+#define dhtType DHT22
+
+
+//Declaring libraries.
+DHT dht(dhtPin, dhtType); 
+
+
+//Declaring variables.
+float tempVal;
+
+
+// Define pin modes (input or output) and serial monitor for debugging. 
+// Will run only once.
 void setup() {
-  pinMode(led, OUTPUT);
+
+  //Initializing libraries.
+  dht.begin();
+
+  //Baud rate is 9600.
+  Serial.begin(9600);
+
 }
 
 void loop() {
-  
-  digitalWrite(led,HIGH);
-  delay(1000);
-  digitalWrite(led,LOW);
-  delay(1000);
-
+  tempVal = dht.readTemperature();
+  Serial.print("Temperature Value : ");
+   Serial.println(tempVal);
 
 }
