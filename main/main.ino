39a57c676f25d6d7342a61eb5ace4c008c5728c3
ATOIsus <sabinmaharjan884@gmail.com>
2023-05-06 03:35:26 +0545

    Test4: Code for SIM900 integrated but testing left.

--- main/main.ino
+++ main/main.ino
@@ -1,32 +1,61 @@
-//Importing libraries.
-#include <DHT.h>;
-//For GPS
-#include <SoftwareSerial.h>
-#include <TinyGPS.h>
+
+//Defining libraries.
+#define dhtType DHT22
+#define TINY_GSM_MODEM_SIM900
+#define SerialMon Serial
+
+#define TOKEN "token"
+#define THINGSBOARD_SERVER "ip"
+#define THINGSBOARD_PORT port
 
 //Defining Pins.
 #define dhtPin 2
 #define gpsRx 7
 #define gpsTx 6
 
-//Defining libraries.
-#define dhtType DHT22
-
-//Declaring libraries.
-DHT dht(dhtPin, dhtType);
-TinyGPS gps;
-SoftwareSerial ss(gpsTx, gpsRx);
+//Importing libraries.
+#include <DHT.h>;
+#include <SoftwareSerial.h>
+#include <TinyGPS.h>
+#include <TinyGsmClient.h>
+#include "ThingsBoard.h"
+#include <StreamDebugger.h>
 
 //Initializing variables.
 float tempVal = 0.0;
 float latVal = 0.0;
 float lonVal = 0.0;
 
+//Initialization for GSM.
+const char apn[] = "web";
+const char user[] = "";
+const char pass[] = "";
+const char server[] = "103.90.84.161";
+
+//For debugging.
+#ifdef DUMP_AT_COMMANDS
+StreamDebugger debugger(SerialMon, SerialMon);
+TinyGsm modem(debugger);
+#else
+TinyGsm modem(SerialMon);
+#endif
+
+#ifdef USE_SSL
+TinyGsmClientSecure client(modem);
+const int port = 443;
+#else
+TinyGsmClient client(modem);
+const int port = 8080;
+#endif
+
+//Creating objects.
+DHT dht(dhtPin, dhtType);
+TinyGPS gps;
+SoftwareSerial ss(gpsTx, gpsRx);
+ThingsBoardHttp tb(client, TOKEN, server, port);
 
 //Declaring functions.
-static void smartdelay(unsigned long ms);
-static void print_float(float val, float invalid, int len, int prec);
-
+static void smartDelay(unsigned long ms);
 
 // Define pin modes (input or output) and serial monitor for debugging.
 // Will run only once.
@@ -34,22 +63,61 @@ void setup() {
 
   //Initializing libraries.
   dht.begin();
-
-  //Baud rate is 9600.
-  Serial.begin(115200);
-
-  //Software Serial for GPS.
+  SerialMon.begin(115200);
   ss.begin(9600);
+
+  //Setting baud rate of GSM module.
+  SerialMon.write("AT+IPR=4800;at&w\r\n");
+  SerialMon.end();
+
+  delay(1000);
+  
+  //Begining serial monitor & GSM module at baud rate 4800.
+  SerialMon.begin(4800);
+  delay(3000);
+
+  SerialMon.println(F("Initializing modem..."));
+  modem.restart();
+  
+  String modemInfo = modem.getModemInfo();
+  SerialMon.print(F("Modem: "));
+  SerialMon.println(modemInfo);
 }
 
 void loop() {
 
+  //Getting temperature data.
   tempVal = dht.readTemperature();
   Serial.print("Temperature Value : ");
   Serial.println(tempVal);
 
+  //Getting latitude and longitude.
   gps.f_get_position(&latVal, &lonVal);
 
+  //Trying to establsh connection with the network provider.
+  SerialMon.print(F("Waiting for network..."));
+  if (!modem.waitForNetwork()) {
+    SerialMon.println(" fail");
+    delay(1000);
+    return;
+  }
+  SerialMon.println(" OK");
+
+  //Trying to establsh connection with the server. 
+  SerialMon.print(F("Connecting to "));
+  SerialMon.print(apn);
+  if (!modem.gprsConnect(apn, user, pass)) {
+    SerialMon.println(" fail");
+    delay(1000);
+    return;
+  }
+  SerialMon.println(" OK");
+  
+  //Sending data to thingsboard server.
+  Serial.println("Sending  data...");
+  tb.sendTelemetryFloat("latitude", latVal);
+  tb.sendTelemetryFloat("longitude", lonVal);
+  tb.sendTelemetryFloat("temperature", tempVal);
 
   Serial.print("Latitude : ");
   Serial.print(latVal);
@@ -57,13 +125,12 @@ void loop() {
   Serial.print(" ,Longitude : ");
   Serial.println(lonVal);
 
-
-  smartdelay(1000);
+  smartDelay(1000);
 }
 
 
-
-static void smartdelay(unsigned long ms) {
+//Function to read gps data for 1 second.
+static void smartDelay(unsigned long ms) {
   unsigned long start = millis();
   do {
     while (ss.available())
